66ece0dfeb26b9c7d8abe7c73b98f14f5a3cc96b
Jonathan Garcia <jonathangarcia@Jonathans-MacBook-Air.local>
2023-11-22 22:43:31 -0600

    Application sin base de datos conectada

---
+++ redbrow/Data/ApplicationDbContext.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace redbrow.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        protected readonly IConfiguration Configuration;
+        public ApplicationDbContext( IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseNpgsql(Configuration.GetConnectionString("WebDatabase"));
+        }
+
+        public DbSet<Usuario> Usuarios { get; set; }
+    }
+}
---
+++ redbrow/Program.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore;
+using redbrow.Data;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+var connectionString = builder.Configuration.GetConnectionString("WebDatabase");
+builder.Services.AddDbContext<ApplicationDbContext>(options =>
+    options.UseNpgsql(connectionString));
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
+
